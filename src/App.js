d01cd6fa326dc75536a6de6e4832afb48251e3ab
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2023-03-11 16:31:59 -0800

    added assets and basic App layout

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => (
+    <div>
+        <h1>
+            <Navbar />
+            <Header />
+            <AboutUs />
+            <SpecialMenu />
+            <Chef />
+            <Intro />
+            <Laurels />
+            <Gallery />
+            <FindUs />
+            <Footer />
+        </h1>
+    </div>
+)
+
+export default App;
